f2c1a3ddbd5640aa5edec1e45b64b19019b7268c
Kale Kundert <kale@thekunderts.net>
2017-11-09 08:38:15 -0800

    Add a paragraph on figure 2d.

--- supp_mat/make_graphics.sh
+++ supp_mat/make_graphics.sh
@@ -2,5 +2,14 @@
 set -euo pipefail
 source ../fcm.sh
 
+function predict_spacer_quality () {(
+    supp_mat=$(pwd)
+    cd -P ../../../notebook/20170329_test_multiple_spacers
+    ./predict_spacer_quality.py -o ${supp_mat}/affinity_correlation.pdf
+)}
+
+
 fcm fold_change 20160304_combined_rational_designs_with_paper_labels.yml \
-    -o $.pdf -d 6e-4,6e0 -p
+    -o in_vitro_in_vivo.pdf -d 6e-4,6e0 -p
+
+predict_spacer_quality
