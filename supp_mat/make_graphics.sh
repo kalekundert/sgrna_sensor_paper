b2331ac51bc5982a5c6cf5f88f813fc3303907b7
Kale Kundert <kale@thekunderts.net>
2017-11-11 06:13:19 -0800

    Add a paragraph about the J23150 experiment.

--- supp_mat/make_graphics.sh
+++ supp_mat/make_graphics.sh
@@ -8,8 +8,16 @@ function predict_spacer_quality () {(
     ./predict_spacer_quality.py -o ${supp_mat}/affinity_correlation.pdf
 )}
 
+#predict_spacer_quality
+#
+#
+#fcm fold_change 20160304_combined_rational_designs_with_paper_labels.yml \
+#    -o in_vitro_in_vivo.pdf \
+#    -d 6e-4,6e0 \
+#    -p
 
-fcm fold_change 20160304_combined_rational_designs_with_paper_labels.yml \
-    -o in_vitro_in_vivo.pdf -d 6e-4,6e0 -p
+fcm fold_change 20171010_compare_promoters.yml \
+    -o graphics/j23150.svg \
+    -I \
+    -d 1e-3,1e1
 
-predict_spacer_quality
