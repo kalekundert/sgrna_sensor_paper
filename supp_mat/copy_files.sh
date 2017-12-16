b481e791c8c87a4ede95b270fdbc2f55314cb63b
Kale Kundert <kale@thekunderts.net>
2017-12-15 16:33:19 -0800

    Tweak the mutagenesis supplemental figure.

--- supp_mat/copy_files.sh
+++ supp_mat/copy_files.sh
@@ -2,4 +2,6 @@
 set -euo pipefail
 
 notebook=../../../notebook/
+
 cp $notebook/20170824_tabulate_in_vitro_screen_data/full_tabular_with_names.tex rational_design_sequences.tex
+cp $notebook/20170227_test_m11_w11_sensors/paper/20170315_test_w11_m11_figure.pdf mutagenesis.pdf
