e488637bd2d7687eb52bfb57059cc80eb94e7c9a
Kale Kundert <kale@thekunderts.net>
2019-01-10 14:44:27 -0500

    Update figure 4 files.

--- figure_4/analyze_data.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-set -euo pipefail
-
-fcm=../../../flow_cytometry
-
-$fcm/fold_change.py \
-    $fcm/data/20170315_test_w11_m11.yml \
-    -p -m mode -O 7x9 -o $.svg -I -n SSC-A
-    
