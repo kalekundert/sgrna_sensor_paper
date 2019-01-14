c1f974824ee9888dcdb8d0569cc67f9a92cb0ba9
Kale Kundert <kale@thekunderts.net>
2019-01-14 14:29:21 -0500

    Make Fig 3 two-column

--- figure_3/make_graphics.sh
+++ figure_3/make_graphics.sh
@@ -7,11 +7,11 @@ notebook=../../../notebook
 lacz=$notebook/20180621_target_endogenous_loci
 pa14=$notebook/20180711_test_ligrnas_in_pseudomonas_aeruginosa
 
-$lacz/miller_units.py \
-  $lacz/plate_reader/20180809_target_lac.toml \
-  $lacz/plate_reader/20180810_target_lac.toml \
-  $lacz/plate_reader/20180813_target_lac.toml \
-  --output 20180809_target_lac_triplicate \
-  --figure-mode 
+#$lacz/miller_units.py \
+#  $lacz/plate_reader/20180809_target_lac.toml \
+#  $lacz/plate_reader/20180810_target_lac.toml \
+#  $lacz/plate_reader/20180813_target_lac.toml \
+#  --output 20180809_target_lac_triplicate \
+#  --figure-mode 
 
-#$pa14/plot_ligrna.py -o graphics/20180711_test_ligrnas_in_pa14.svg
+$pa14/plot_ligrna.py -o graphics/20180711_test_ligrnas_in_pa14.svg
